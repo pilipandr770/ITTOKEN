624140601b074cd718abcd0c030b4e88316718e8
Andrii Pylypchuk <161452940+pilipandr770@users.noreply.github.com>
2025-06-01 18:35:29 +0000

    Оновлення структури: видалено дублікати run.py/wsgi.py, залишено тільки app/

--- app/wsgi.py
+++ app/wsgi.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 """
-Файл для продакшн-запуска приложения через Gunicorn WSGI-сервер.
+Файл для продакшн-запуска додатку через Gunicorn WSGI-сервер.
 """
 
 from app import create_app
 
-application = create_app()
+app = create_app()
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
